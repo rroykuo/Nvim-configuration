0a954a5971641e1fcb1ad8799bd633b6b2369758
rroykuo <rroykuo@gmail.com>
2024-05-11 15:28:06 +0800

    add tokyo night config

---
+++ lua/plugins/tokyonight.lua
@@ -0,0 +1,25 @@
+return {
+	{
+		"folke/tokyonight.nvim",
+		priority = 1000, -- make sure to load this before all the other start plugins
+		config = function()
+			require("tokyonight").setup({
+				style = "night",
+				transparent = true,
+				styles = {
+					-- Style to be applied to different syntax groups
+					-- Value is any valid attr-list value for `:help nvim_set_hl`
+					comments = { italic = true },
+					keywords = { italic = true },
+					functions = {},
+					variables = {},
+					-- Background styles. Can be "dark", "transparent" or "normal"
+					sidebars = "transparent", -- style for sidebars, see below
+					floats = "transparent", -- style for floating windows
+				},
+			})
+			-- load the colorscheme here
+			vim.cmd([[colorscheme tokyonight]])
+		end,
+	},
+}
