071a3af19758b80efe5e63286bdc05b9fd6eaeda
rroykuo <rroykuo@gmail.com>
2024-05-13 02:18:06 +0800

    add formatting lua

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,42 +1,44 @@
 return {
-	"nvim-treesitter/nvim-treesitter",
-	-- ensure these language parsers are installed
-	opts = {
-		ensure_installed = {
-			"bash",
-			"c",
-			"cmake",
-			"cpp",
-			"cuda",
-			"dockerfile",
-			"gitignore",
-			"lua",
-			"make",
-			"markdown",
-			"markdown_inline",
-			"python",
-			"yaml",
+	{
+		"nvim-treesitter/nvim-treesitter",
+		-- ensure these language parsers are installed
+		opts = {
+			ensure_installed = {
+				"bash",
+				"c",
+				"cmake",
+				"cpp",
+				"cuda",
+				"dockerfile",
+				"gitignore",
+				"lua",
+				"make",
+				"markdown",
+				"markdown_inline",
+				"python",
+				"yaml",
+			},
+			auto_install = true,
+			highlight = {
+				enable = true,
+			},
+			autopairs = {
+				enable = true,
+			},
+			indent = {
+				enable = true,
+			},
+			context_commetstring = {
+				enable = true,
+			},
+			-- Or use a function for more flexibility, e.g. to disable slow treesitter highlight for large files
+			disable = function(lang, buf)
+				local max_filesize = 100 * 1024 -- 100 KB
+				local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
+				if ok and stats and stats.size > max_filesize then
+					return true
+				end
+			end,
 		},
-		auto_install = true,
-		highlight = {
-			enable = true,
-		},
-		autopairs = {
-			enable = true,
-		},
-		indent = {
-			enable = true,
-		},
-		context_commetstring = {
-			enable = true,
-		},
-		-- Or use a function for more flexibility, e.g. to disable slow treesitter highlight for large files
-		disable = function(lang, buf)
-			local max_filesize = 100 * 1024 -- 100 KB
-			local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
-			if ok and stats and stats.size > max_filesize then
-				return true
-			end
-		end,
 	},
 }
