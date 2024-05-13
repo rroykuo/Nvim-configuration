1346068027e53af84a5420f35994c636c174107a
rroykuo <rroykuo@gmail.com>
2024-05-14 02:10:09 +0800

    modified notify.lua

---
+++ lua/plugins/notify.lua
@@ -0,0 +1,10 @@
+return {
+	{
+		"folke/noice.nvim",
+		opts = {
+			presets = {
+				command_palette = false,
+			},
+		},
+	},
+}
--- lua/plugins/tokyonight.lua
+++ lua/plugins/tokyonight.lua
@@ -1,25 +1,20 @@
 return {
 	{
 		"folke/tokyonight.nvim",
-		priority = 1000, -- make sure to load this before all the other start plugins
-		config = function()
-			require("tokyonight").setup({
-				style = "night",
-				transparent = true,
-				styles = {
-					-- Style to be applied to different syntax groups
-					-- Value is any valid attr-list value for `:help nvim_set_hl`
-					comments = { italic = true },
-					keywords = { italic = true },
-					functions = {},
-					variables = {},
-					-- Background styles. Can be "dark", "transparent" or "normal"
-					sidebars = "transparent", -- style for sidebars, see below
-					floats = "transparent", -- style for floating windows
-				},
-			})
-			-- load the colorscheme here
-			vim.cmd([[colorscheme tokyonight]])
-		end,
+		opts = {
+			style = "night",
+			transparent = true,
+			styles = {
+				-- Style to be applied to different syntax groups
+				-- Value is any valid attr-list value for `:help nvim_set_hl`
+				comments = { italic = true },
+				keywords = { italic = true },
+				functions = {},
+				variables = {},
+				-- Background styles. Can be "dark", "transparent" or "normal"
+				sidebars = "transparent", -- style for sidebars, see below
+				floats = "transparent", -- style for floating windows
+			},
+		},
 	},
 }
