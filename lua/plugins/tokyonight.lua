91815c24d32e935996779b99d6a97878a8cea71a
rroykuo <rroykuo@gmail.com>
2024-05-14 12:56:34 +0800

    modified nvim-cmp.lua

--- lua/plugins/tokyonight.lua
+++ lua/plugins/tokyonight.lua
@@ -1,20 +1,18 @@
 return {
-	{
-		"folke/tokyonight.nvim",
-		opts = {
-			style = "night",
-			transparent = true,
-			styles = {
-				-- Style to be applied to different syntax groups
-				-- Value is any valid attr-list value for `:help nvim_set_hl`
-				comments = { italic = true },
-				keywords = { italic = true },
-				functions = {},
-				variables = {},
-				-- Background styles. Can be "dark", "transparent" or "normal"
-				sidebars = "transparent", -- style for sidebars, see below
-				floats = "transparent", -- style for floating windows
-			},
+	"folke/tokyonight.nvim",
+	opts = {
+		style = "night",
+		transparent = true,
+		styles = {
+			-- Style to be applied to different syntax groups
+			-- Value is any valid attr-list value for `:help nvim_set_hl`
+			comments = { italic = true },
+			keywords = { italic = true },
+			functions = {},
+			variables = {},
+			-- Background styles. Can be "dark", "transparent" or "normal"
+			sidebars = "transparent", -- style for sidebars, see below
+			floats = "transparent", -- style for floating windows
 		},
 	},
 }
