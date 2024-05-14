377957ef816925f03fb423ac6f4f9a34f797e3bb
rroykuo <rroykuo@gmail.com>
2024-05-14 16:48:54 +0800

    config c++ & python for lsp

---
+++ lua/plugins/linting.lua
@@ -0,0 +1,9 @@
+return {
+	"mfussenegger/nvim-lint",
+	opts = {
+		linters_by_ft = {
+			cpp = { "cpplink" },
+			python = { "pylint" },
+		},
+	},
+}
---
+++ lua/plugins/lspconfig.lua
@@ -0,0 +1,13 @@
+return {
+	"neovim/nvim-lspconfig",
+	opts = {
+		setup = {
+			clangserver = function(_, opts)
+				require("clangd").setup({ server = opts, filetypes = { "c", "cpp", "objc", "objcpp" } })
+			end,
+			pythonserver = function(_, opts)
+				require("basedpyright").setup({ server = opts, filetypes = { "python" } })
+			end,
+		},
+	},
+}
