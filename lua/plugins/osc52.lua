2388a3280f8db990c98b75640d34da4de49ae3ec
rroykuo <rroykuo@gmail.com>
2024-05-18 21:49:01 +0800

    osc52 update

--- lua/plugins/osc52.lua
+++ lua/plugins/osc52.lua
@@ -1,25 +1,19 @@
 return {
   "ojroques/nvim-osc52",
-  opts = function(_, _)
+  opts = function()
     local function copy(lines, _)
       require('osc52').copy(table.concat(lines, '\n'))
     end
+
     local function paste()
-      require { vim.fn.split(vim.fn.getreg(''), '\n'), vim.fn.getregtype('') }
+      return { vim.fn.split(vim.fn.getreg(''), '\n'), vim.fn.getregtype('') }
     end
+
     vim.g.clipboard = {
       name = 'osc52',
       copy = { ['+'] = copy, ['*'] = copy },
       paste = { ['+'] = paste, ['*'] = paste },
     }
+    vim.keymap.set('v', '<leader>y', '"+y', { desc = "Copy to clipboard (OSC52)" })
   end,
-  -- opts = function(_, _)
-  --   function copy()
-  --     if vim.v.event.operator == 'y' and vim.v.event.regname == '+' then
-  --       require('osc52').copy_register('+')
-  --     end
-  --   end
-  --
-  --   vim.api.nvim_create_autocmd('TextYankPost', { callback = copy })
-  -- end,
 }
