715af05d697cc9fa7978089d4513137d7a108ee6
rroykuo <rroykuo@gmail.com>
2024-05-18 21:06:20 +0800

    add osc52

--- lua/plugins/osc52.lua
+++ lua/plugins/osc52.lua
@@ -1,3 +1,25 @@
 return {
-    "ojroques/nvim-osc52",
+  "ojroques/nvim-osc52",
+  opts = function(_, _)
+    local function copy(lines, _)
+      require('osc52').copy(table.concat(lines, '\n'))
+    end
+    local function paste()
+      require { vim.fn.split(vim.fn.getreg(''), '\n'), vim.fn.getregtype('') }
+    end
+    vim.g.clipboard = {
+      name = 'osc52',
+      copy = { ['+'] = copy, ['*'] = copy },
+      paste = { ['+'] = paste, ['*'] = paste },
+    }
+  end,
+  -- opts = function(_, _)
+  --   function copy()
+  --     if vim.v.event.operator == 'y' and vim.v.event.regname == '+' then
+  --       require('osc52').copy_register('+')
+  --     end
+  --   end
+  --
+  --   vim.api.nvim_create_autocmd('TextYankPost', { callback = copy })
+  -- end,
 }
