527be6eab3302f7dc2c2e28811ac9de526b345a2
rroykuo <rroykuo@gmail.com>
2024-05-20 00:06:46 +0800

    add lazygit

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -20,7 +20,6 @@ require("lazy").setup({
         { import = "lazyvim.plugins.extras.lang.markdown" },
         { import = "lazyvim.plugins.extras.lang.python" },
         { import = "lazyvim.plugins.extras.lang.yaml" },
-        { import = "lazyvim.plugins.extras.util.gitui" },
         { import = "lazyvim.plugins.extras.coding.codeium" },
         -- import/override with your plugins
         { import = "plugins" },
