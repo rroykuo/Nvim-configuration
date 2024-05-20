6e4311ae1327d3d202d944ae71a019542c4b060a
rroykuo <rroykuo@gmail.com>
2024-05-21 01:29:46 +0800

    add cmake lsp

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -16,6 +16,7 @@ require("lazy").setup({
         { import = "lazyvim.plugins.extras.formatting.prettier" },
         { import = "lazyvim.plugins.extras.lang.ansible" },
         { import = "lazyvim.plugins.extras.lang.clangd" },
+        { import = "lazyvim.plugins.extras.lang.cmake" },
         { import = "lazyvim.plugins.extras.lang.docker" },
         { import = "lazyvim.plugins.extras.lang.markdown" },
         { import = "lazyvim.plugins.extras.lang.python" },
