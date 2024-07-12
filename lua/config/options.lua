fe1b2e9009e8c4f1b1bae709c93e541d0909e2f3
rroykuo <rroykuo@gmail.com>
2024-07-12 16:13:32 +0800

    update treesitter

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,8 +1,9 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
--- local opt = vim.opt
--- opt.shiftwidth = 4 -- Size of an indents
--- opt.tabstop = 4 -- Number of spaces tabs count for
 vim.g.lazyvim_python_lsp = "pyright"
 vim.g.lazyvim_python_ruff = "ruff_lsp"
+vim.opt.tabstop = 2 -- A TAB character looks like 4 spaces
+vim.opt.expandtab = true -- Pressing the TAB key will insert spaces instead of a TAB character
+vim.opt.softtabstop = 2 -- Number of spaces inserted instead of a TAB character
+vim.opt.shiftwidth = 2 -- Number of spaces inserted when indenting
--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -5,7 +5,7 @@ return {
         highlight = {
             enable = true,
             disable = function(_, buf)
-                local max_filesize = 10 * 1024 -- 10 KB
+                local max_filesize = 10 * 1024 -- 10 MB
                 local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
                 if ok and stats and stats.size > max_filesize then
                     return true
@@ -16,7 +16,7 @@ return {
             enable = true,
         },
         indent = {
-            enable = true,
+            enable = false,
         },
         context_commetstring = {
             enable = true,
